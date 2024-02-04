73a35865c2f999711f140add2c4b30fa92012bff
Ojas <singhojas3299@gmail.com>
2024-02-04 21:30:52 +0530

    2nd page done

--- src/components/DetailPage.js
+++ src/components/DetailPage.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
+import "../App.css";
 
 const DetailPage = () => {
   const { id } = useParams();
-
 
   // Fetch the specific item details based on the ID
   // You can use Redux, a separate API call, or any other method to fetch the data.
@@ -14,21 +14,31 @@ const DetailPage = () => {
 
   // const postDetails = useSelector((state) => getPostDetailsById(state, id));
   const postDetails = useSelector((state) =>
-  state.data.find((post) => post.id === parseInt(id))
-);
-if (!postDetails) {
+    state.data.find((post) => post.id === parseInt(id))
+  );
+  if (!postDetails) {
     // Handle the case when post details are not found
     return <div>Loading...</div>;
   }
-console.log(postDetails)
+  console.log(postDetails);
 
   return (
     <div className="detail-page">
-      {/* Display post details */}
-      {/* Replace the following with your actual data */}
-      <h2>{postDetails.title}</h2>
-      <p>{postDetails.body}</p>
-      <p>User ID: {id}</p>
+      <h1>Post Number : {id} </h1>
+      <div className="detailCard">
+        
+          <img src={`https://picsum.photos/200?random=${postDetails.id}`} />
+          <div>
+               <p className="about">{postDetails.body}
+               {postDetails.body}
+               {postDetails.body}
+               {postDetails.body}</p>
+
+          </div>
+       
+      </div>
+      <h2 >{postDetails.title}</h2>
+      <h1>More Post</h1>
     </div>
   );
 };
