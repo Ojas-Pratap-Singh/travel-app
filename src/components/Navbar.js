9ce16a1e3b4fc2bf9eefe8f96a76a21a51d61616
Ojas <singhojas3299@gmail.com>
2024-02-04 21:47:48 +0530

    done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
 
 
     return (
         <div className="navbar">
-            <div className="logo">TravelMedia.in</div>
+            <div className="logo">
+            <Link to={"/"}>TravelMedia.in</Link></div>
             <div className="searchIcon">
             <i class="fa-solid fa-house"></i>
             <i class="fa-solid fa-bell"></i>
